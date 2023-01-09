fb12f53e45099f40cba5f907413210834eba9d1e
AgueroZZ <aguero.zhang@mail.utoronto.ca>
2023-01-08 23:37:18 -0500

    update ukb european result

--- section 5/European/01_QC.R
+++ section 5/European/01_QC.R
@@ -62,8 +62,11 @@ save(pheno_data, file = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hype
 #################################################
 #################################################
 names_want_pop <- grep("f.21000.0",bd_names,value = T)
-pop_want <- fread("/home/ziang/Documents/UKB/ukb47570.tab", header=TRUE, sep="\t", select = c("f.eid",names_want_pop))
-pop_want_European <- pop_want %>% filter(f.21000.0.0 == 4002)
+names_want_pop_genetic_confirmed <- grep("f.22006",bd_names,value = T)
+names_want_pop_kin <- grep("f.22021.0.0", bd_names,value = T)
+pop_want <- fread("/home/ziang/Documents/UKB/ukb47570.tab", header=TRUE, sep="\t", select = c("f.eid",names_want_pop, names_want_pop_genetic_confirmed, names_want_pop_kin))
+pop_want_European <- pop_want %>% filter(f.21000.0.0 == 1001, !is.na(f.22006.0.0))
+pop_want_European <- pop_want_European %>% filter(f.22021.0.0 == 0)
 European_ID <- pop_want_European$f.eid
 pheno_data <- pheno_data %>% filter(ID %in% European_ID)
 ### Number of self-reported European individuals to start with:
@@ -95,157 +98,30 @@ for (i in 1:22) {
 }
 system(paste0(plink19,
               " --bfile ", "selected_c1_European",
-              " --merge-list ", "merge_list.txt",
+              " --merge-list ", "merge_list_European.txt",
               " --make-bed",
               " --out ", "self_European_merged_data"))
 
-
-
 #####################################################################
 #####################################################################
-##### Step 3: Filter Indivds by KIN and MIND
+##### Step 3: Filter Indivds by MIND
 #####################################################################
 #####################################################################
 system(paste0(plink,
               paste(" --bfile", "self_European_merged_data"),
-              " --king-cutoff 0.25",
               " --mind 0.2",
               " --make-bed ",
               paste(" --out", "self_European_filtered_data")
 ))
-# 0 samples removed due to missing genotype data (--mind).
-# self_European_filtered_data.king.cutoff.out.id , and 3182 remaining sample IDs
-
-
-
-
+# 276682 samples (147522 females, 129160 males; 276682 founders) remaining
 
 #####################################################################
 #####################################################################
-##### Step 4: Filter Indivds by overall PC with Kmeans
+##### Step 4: Filter SNPs and record the number
 #####################################################################
 #####################################################################
-names_want_PC <- grep("f.22009", bd_names,value = T)
-names_want_PCi <- names_want_PC[c(1:4,15)]
-PCi_want <- fread("/home/ziang/Documents/UKB/ukb47570.tab", header=TRUE, sep="\t", select = c("f.eid",names_want_PCi))
-names(PCi_want) <- c("ID", "PC1", "PC2", "PC3", "PC4", "PC15")
-PC_data_full <- left_join(x = PCi_want, y = pheno_data, by = "ID")
-PC_data_full$Ancestry <- ifelse(is.na(PC_data_full$asthma), "Other", "European (Self-Reported)")
-PC_data_full %>% ggplot() + geom_point(aes(x = PC1, y = PC2, color = Ancestry, shape = Ancestry), alpha = 0.3, size = 2) + theme_classic() +
-  xlim(0,500) + theme(legend.position = c(0.8,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/All_PC_Figure12.pdf", device = "pdf")
-PC_data_full %>% ggplot() + geom_point(aes(x = PC3, y = PC4, color = Ancestry, shape = Ancestry), alpha = 0.3, size = 2) + theme_classic()
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/All_PC_Figure34.pdf", device = "pdf")
-pheno_data_merged <- merge(pheno_data, PCi_want, by = "ID")
-select_ID <- read.table("self_European_filtered_data.king.cutoff.in.id")
-select_ID <- select_ID$V1
-### Only use unrelated individuals for clustering
-pheno_data_selected <- pheno_data_merged %>% filter(ID %in% select_ID)
-Restrict_Data <- pheno_data_selected %>% na.omit()
-Restrict_Data12 <- Restrict_Data[, c("PC1", "PC2")]
-set.seed(123)
-kmeans_mod12 <- kmeans(Restrict_Data12, centers = 4, nstart = 100)
-Restrict_Data$Clusters <- as.character(kmeans_mod12$cluster)
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC1, y = PC2, color = Clusters), alpha = 0.3, size = 2) + theme_classic()
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_cluster_Figure12.pdf", device = "pdf")
-table(Restrict_Data$Clusters)/sum(table(Restrict_Data$Clusters))
-largest_Cluster_ID <- which.is.max(table(Restrict_Data$Clusters))
-Selected_ID <- Restrict_Data$ID[Restrict_Data$Clusters == largest_Cluster_ID]
-Cluster_Restrict_Data <- Restrict_Data[Restrict_Data$Clusters == largest_Cluster_ID,]
-nrow(Cluster_Restrict_Data) #2601
-save(Cluster_Restrict_Data, file = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/Cluster_Restrict_Data.rda")
-
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC1, y = PC2, color = Clusters), alpha = 0.3, size = 2) + theme_classic() + xlim(c(0,500)) + theme(legend.position = c(0.9,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_Figure12.pdf", device = "pdf")
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC1, y = PC3, color = Clusters), alpha = 0.3, size = 2) + theme_classic() + xlim(c(0,500)) + theme(legend.position = c(0.9,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_Figure13.pdf", device = "pdf")
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC2, y = PC3, color = Clusters), alpha = 0.3, size = 2) + theme_classic() + xlim(c(-150,100)) + theme(legend.position = c(0.9,0.8))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_Figure23.pdf", device = "pdf")
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC3, y = PC4, color = Clusters), alpha = 0.3, size = 2) 
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_Figure34.pdf", device = "pdf")
-Restrict_Data %>% ggplot() + geom_point(aes(x = PC1, y = PC15, color = Clusters), alpha = 0.3, size = 2) 
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_PC_Figure115.pdf", device = "pdf")
-
-
-
-
-
-
-#####################################################################
-#####################################################################
-##### Step 5: Compute new PCs and remove outliers
-#####################################################################
-#####################################################################
-system(paste0(plink19,
-              " --bfile ", "self_European_filtered_data",
-              " --pca 20 header ",
-              " --make-bed",
-              " --out ", "self_European_PCA"))
-newPCs <- read.table(file = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/self_European_PCA.eigenvec", header = T)
-newPCs$ID <- newPCs$IID
-Cluster_Data <- data.frame(ID = (pheno_data_selected %>% na.omit())$ID, Cluster = as.character(kmeans_mod12$cluster))
-newPCs_clustered <- inner_join(newPCs, Cluster_Data, by = "ID")
-newPCS_cluster1 <- newPCs %>% filter(ID %in% Selected_ID)
-newPCS_cluster1 <- newPCS_cluster1[,-c(1:2)]
-newPCs_clustered %>% ggplot() + geom_point(aes(x = PC1, y = PC2, color = Cluster), alpha = 0.3, size = 2) + theme_classic() + theme(legend.position = c(0.1,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_newPC_Figure12.pdf", device = "pdf")
-newPCs_clustered %>% ggplot() + geom_point(aes(x = PC1, y = PC3, color = Cluster), alpha = 0.3, size = 2) + theme_classic() + theme(legend.position = c(0.1,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_newPC_Figure13.pdf", device = "pdf")
-newPCs_clustered %>% ggplot() + geom_point(aes(x = PC2, y = PC3, color = Cluster), alpha = 0.3, size = 2) + theme_classic() + theme(legend.position = c(0.8,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_newPC_Figure23.pdf", device = "pdf")
-newPCs_clustered %>% ggplot() + geom_point(aes(x = PC3, y = PC4, color = Cluster), alpha = 0.3, size = 2) + theme_classic() + theme(legend.position = c(0.1,0.3))
-ggsave(filename = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/European_newPC_Figure34.pdf", device = "pdf")
-Identify_outliers <- function(all_data, index){
-  PCi <- all_data[,index]
-  ID <- all_data$ID
-  PC_mu <- mean(PCi)
-  PC_sd <- sd(PCi)
-  removed_ID <- ID[PCi>=(PC_mu + 4*PC_sd) | PCi<=(PC_mu - 4*PC_sd)]
-  removed_ID
-}
-ID_to_removes <- c()
-for (i in 1:20) {
-  ID_to_removes <- unique(c(ID_to_removes,Identify_outliers(newPCS_cluster1, index = i)))
-}
-length(ID_to_removes) #91
-final_ID <- newPCS_cluster1$ID[!newPCS_cluster1$ID %in% ID_to_removes]
-length(final_ID) #2510
-final_data <- Cluster_Restrict_Data[Cluster_Restrict_Data$ID %in% final_ID,]
-save(final_data, file = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/final_data.rda")
-
-### Elbow Plot using new PCs:
-newPC_eigenvec <- read.table(file = paste0(data_path, "self_European_PCA.eigenvec"), header = T)
-newPC_eigen <- read.table(file = paste0(data_path, "self_European_PCA.eigenval"), header = F)
-newPC_eigen <- as.data.frame(newPC_eigen)
-newPC_eigen$PC <- 1:nrow(newPC_eigen)
-names(newPC_eigen) <- c("Variance", "PC")
-newPC_eigen$variance_explained <- newPC_eigen$Variance/sum(newPC_eigen$Variance)
-newPC_eigen$cumu_var <- cumsum(newPC_eigen$Variance)
-newPC_eigen %>% ggplot(aes(x = PC, y = variance_explained)) + geom_point() + 
-  geom_line() + 
-  ylab("Variance Percentage") +
-  ggtitle("Percentage of variance explained by each PC") +
-  theme_classic()
-ggsave(filename = paste0(figure_path,"elbow_plot.pdf"), device = "pdf", width = 5,
-       height = 5)
-ggsave(filename = paste0(figure_path,"elbow_plot.png"), device = "png", width = 5,
-       height = 5)
-
-
-
-
-
-
-#####################################################################
-#####################################################################
-##### Step 6: Filter SNPs and record the number
-#####################################################################
-#####################################################################
-matrix_to_write <- cbind(final_ID,final_ID)
-write.table(x = matrix_to_write, file = "/home/ziang/Documents/UKB/final_ID.txt", row.names = F, col.names = F)
 system(paste0(plink,
               paste(" --bfile", "self_European_filtered_data"),
-              " --keep final_ID.txt",
               " --hwe 1e-10",
               " --maf 0.01",
               " --geno 0.2",
@@ -253,10 +129,10 @@ system(paste0(plink,
               paste(" --out", "self_European_filtered_data_afterSNP_QC")
 ))
 # 784256 variants loaded from self_European_filtered_data.bim.
-# 384645 variants removed due to allele frequency threshold(s)
-# --hwe: 2603 variants removed due to Hardy-Weinberg exact test (founders only).
-# --geno: 18005 variants removed due to missing genotype data.
-# 379003 variants remaining after main filters.
+# 123610 variants removed due to allele frequency threshold(s)
+# 17934 variants removed due to missing genotype data
+# --hwe: 41037 variants removed due to Hardy-Weinberg exact test (founders only).
+# 601675 variants remaining after main filters.
 
 
 
--- section 5/European/03_Computation.R
+++ section 5/European/03_Computation.R
@@ -1,4 +1,4 @@
-#### Output the full gwas result of hypertension on african
+#### Output the full gwas result of hypertension on European
 library(stringr)
 library(data.table)
 library(tidyverse)
@@ -12,26 +12,27 @@ library(HardyWeinberg)
 library(SPCompute)
 
 geno_path = "/home/ziang/Documents/UKB/"
-data_path = "/home/ziang/Documents/Stats-Gene/UKB/African_GWAS_hypertension_bp/data/"
+data_path = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/"
 plink = "/home/ziang/Documents/UKB/plink2"
 plink19 = "/home/ziang/Documents/UKB/plink19"
-result_path = "/home/ziang/Documents/Stats-Gene/UKB/African_GWAS_hypertension_bp/results/"
-figure_path = "/home/ziang/Documents/Stats-Gene/UKB/African_GWAS_hypertension_bp/figures/"
+result_path = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/results/"
+figure_path = "/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/"
 
 
 # geno_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB"
-# data_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/African_GWAS_hypertension_bp/data/"
-# result_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/African_GWAS_hypertension_bp/results/"
-# figure_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/African_GWAS_hypertension_bp/figures/"
+# data_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/"
+# result_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/European_GWAS_hypertension_bp/results/"
+# figure_path = "/Users/ziangzhang/Documents/GitHub/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/"
 
 
 
 # geno_path = "D:/Stats-Gene/UKB/"
-# data_path = "D:/Stats-Gene/UKB/African_GWAS_hypertension_bp/data/"
-# result_path = "D:/Stats-Gene/UKB/African_GWAS_hypertension_bp/results/"
-# figure_path = "D:/Stats-Gene/UKB/African_GWAS_hypertension_bp/figures/"
+# data_path = "D:/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/"
+# result_path = "D:/Stats-Gene/UKB/European_GWAS_hypertension_bp/results/"
+# figure_path = "D:/Stats-Gene/UKB/European_GWAS_hypertension_bp/figures/"
 
-load(paste0(data_path, "final_data.rda"))
+load("/home/ziang/Documents/Stats-Gene/UKB/European_GWAS_hypertension_bp/data/pheno_data.rda")
+final_data <- pheno_data[,-2] %>% na.omit()
 colnames(final_data)[1] <- "IID"
 final_data <- cbind(FID = final_data$IID ,final_data)
 final_data$hypertension <- ifelse(final_data$hypertension == 1, 2,1)
@@ -41,9 +42,9 @@ load(paste0(data_path, "result_gamma_sex.rda"))
 load(paste0(data_path, "result_gamma_age.rda"))
 load(paste0(data_path, "top_result_bp_summary.rda"))
 load(paste0(data_path, "top_result_hyper_summary.rda"))
-MAF_bp <- fread(file = paste0(result_path, "self_africa_result_bp.afreq"))
+MAF_bp <- fread(file = paste0(result_path, "self_European_result_bp.afreq"))
 MAF_bp <- MAF_bp %>% filter(ID %in% top_result_bp_summary$SNP) %>% mutate(SNP = ID, MAF = ALT_FREQS) %>% select(SNP, MAF)
-MAF_hyper <- fread(file = paste0(result_path, "self_africa_result_hyper.afreq"))
+MAF_hyper <- fread(file = paste0(result_path, "self_European_result_hyper.afreq"))
 MAF_hyper <- MAF_hyper %>% filter(ID %in% top_result_hyper_summary$SNP) %>% mutate(SNP = ID, MAF = ALT_FREQS) %>% select(SNP, MAF)
 Gamma_sex_bp <- result_gamma_sex %>% filter(ID %in% top_result_bp_summary$SNP) %>% mutate(SNP = ID) %>% select(SNP, Gamma_Sex)
 Gamma_sex_hyper <- result_gamma_sex %>% filter(ID %in% top_result_hyper_summary$SNP) %>% mutate(SNP = ID) %>% select(SNP, Gamma_Sex)
@@ -65,7 +66,7 @@ result <- c()
 SNP_chro <- top_result_bp_summary$CHR
 for (i in 1:length(SNP_chro)) {
   system(paste0(plink,
-                " --bfile ", geno_path,"self_africa_filtered_data_afterSNP_QC",
+                " --bfile ", geno_path,"self_European_filtered_data_afterSNP_QC",
                 " --snp ", SNP_names[i],
                 " --make-bed ",
                 " --out ", result_path, "top_SNP_bp"))
@@ -97,7 +98,7 @@ for (i in 1:length(top_result_bp_summary$SNP)) {
 
 #####################################################################
 #####################################################################
-##### Step 12: Power Computation for the finished GWAS
+##### Step 10: Power Computation for the finished GWAS
 #####################################################################
 #####################################################################
 Power_bp_with_E <- c()
@@ -122,7 +123,7 @@ bp_result %>% pivot_longer(cols = c(Power_without_E, Power_withE), values_to = "
   xlab("SNP") +
   scale_fill_manual(labels = c("with E", "without E"), values = c("2", "4")) +
   ggtitle("Blood Pressure") +
-  coord_cartesian(ylim=c(0.1,0.6)) +
+  coord_cartesian(ylim=c(0.9,1.0)) +
   theme_classic()
 
 ggsave(filename = paste0(figure_path, "bp_barplot.pdf"), device = "pdf", width = 5,
@@ -152,7 +153,7 @@ hypertension_result %>% pivot_longer(cols = c(Power_without_E, Power_withE), val
   xlab("SNP") +
   scale_fill_manual(labels = c("with E", "without E"), values = c("2", "4")) +
   ggtitle("Hypertension") +
-  coord_cartesian(ylim=c(0.1,0.6)) +
+  coord_cartesian(ylim=c(0.9,1)) +
   theme_classic()
 
 ggsave(filename = paste0(figure_path, "hypertension_barplot.pdf"), device = "pdf", width = 5,
@@ -166,7 +167,7 @@ ggsave(filename = paste0(figure_path, "hypertension_barplot.pdf"), device = "pdf
 
 #####################################################################
 #####################################################################
-##### Step 13: Power Computation for the replication GWAS
+##### Step 11: Power Computation for the replication GWAS
 #####################################################################
 #####################################################################
 Power_bp_with_E <- c()
@@ -232,7 +233,7 @@ ggsave(filename = paste0(figure_path, "hypertension_barplot_replicate.pdf"), dev
 
 #####################################################################
 #####################################################################
-##### Step 14: Sample Size Computation for the replication GWAS
+##### Step 12: Sample Size Computation for the replication GWAS
 #####################################################################
 #####################################################################
 Size_bp_with_E <- c()
@@ -257,7 +258,7 @@ bp_result %>% pivot_longer(cols = c(Size_without_E, Size_withE), values_to = "Sa
   xlab("SNP") +
   scale_fill_manual(labels = c("with E", "without E"), values = c("2", "4")) +
   ggtitle("Blood Pressure") +
-  coord_cartesian(ylim=c(1000,2000)) +
+  coord_cartesian(ylim=c(100000,130000)) +
   theme_classic()
 
 ggsave(filename = paste0(figure_path, "bp_barplot_size.pdf"), device = "pdf", width = 5,
@@ -287,7 +288,7 @@ hypertension_result %>% pivot_longer(cols = c(Size_without_E, Size_withE), value
   xlab("SNP") +
   scale_fill_manual(labels = c("with E", "without E"), values = c("2", "4")) +
   ggtitle("Hypertension") +
-  coord_cartesian(ylim=c(1000, 2000)) +
+  coord_cartesian(ylim=c(100000,130000)) +
   theme_classic()
 
 ggsave(filename = paste0(figure_path, "hypertension_barplot_size.pdf"), device = "pdf", width = 5,
@@ -295,7 +296,7 @@ ggsave(filename = paste0(figure_path, "hypertension_barplot_size.pdf"), device =
 
 
 
-
+### The computed powers are too big... there should be some errors or bugs.
 
 
 
